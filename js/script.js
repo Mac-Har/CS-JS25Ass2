353d3dee30c51da6b2cdcdfef41f6c29755618fd
Mac-Har <200615255@student.georgianc.on.ca>
2025-07-14 15:39:47 -0400

    Functional script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 class Smoothie {
     ingredients = [];
     size;
+
     constructor(size) {
         this.size = size;
     }
+    addIngredient(ingredient)
+    {
+        this.ingredients.push(ingredient);
+    }
+}
+
+let smoothie = new Smoothie("Large");
+
+const output = document.querySelector("#output");
+const addbanana = document.querySelector("#addbanana");
+
+addbanana.addEventListener("click", banana)
+function refreshOutput(smooth)
+{
+    output.textContent = "Your smoothie is " + smooth.size + ", containing" + smooth.ingredients.join();
+}
+
+function banana()
+{
+    smoothie.addIngredient("a banana \n");
+    refreshOutput(smoothie);
 }
